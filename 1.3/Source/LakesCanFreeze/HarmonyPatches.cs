4ddd472e8be807a0a1f482fd064b35f6cabfbd4c
Evelyn <udderlyevelyn@gmail.com>
2021-12-21 23:49:01 -0500

    Completed bulk of content.

--- 1.3/Source/LakesCanFreeze/HarmonyPatches.cs
+++ 1.3/Source/LakesCanFreeze/HarmonyPatches.cs
@@ -9,87 +9,144 @@ using HarmonyLib;
 using Verse.Noise;
 using RimWorld.Planet;
 using UnityEngine;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace LCF
 {
-	[HarmonyPatch(typeof(GenStep_ElevationFertility), "Generate")]
-	class HarmonyPatches
-	{
-		static bool Prefix(Map map, GenStepParams parms)
-		{
-			NoiseRenderer.renderSize = new IntVec2(map.Size.x, map.Size.z);
-			ModuleBase input = new Perlin(0.020999999716877937, 2.0, 0.5, 6, map.ConstantRandSeed, QualityMode.High);
-			input = new ScaleBias(0.5, 0.5, input);
-			NoiseDebugUI.StoreNoiseRender(input, "elev base");
-			float num = 1f;
-			switch (map.TileInfo.hilliness)
-			{
-				case Hilliness.Flat:
-					num = MapGenTuning.ElevationFactorFlat;
-					break;
-				case Hilliness.SmallHills:
-					num = MapGenTuning.ElevationFactorSmallHills;
-					break;
-				case Hilliness.LargeHills:
-					num = MapGenTuning.ElevationFactorLargeHills;
-					break;
-				case Hilliness.Mountainous:
-					num = MapGenTuning.ElevationFactorMountains;
-					break;
-				case Hilliness.Impassable:
-					num = MapGenTuning.ElevationFactorImpassableMountains;
-					break;
-			}
-			input = new Multiply(input, new Const(num));
-			NoiseDebugUI.StoreNoiseRender(input, "elev world-factored");
-			if (map.TileInfo.hilliness == Hilliness.Mountainous || map.TileInfo.hilliness == Hilliness.Impassable)
-			{
-				ModuleBase input2 = new DistFromAxis((float)map.Size.x * 0.42f);
-				input2 = new Clamp(0.0, 1.0, input2);
-				input2 = new Invert(input2);
-				input2 = new ScaleBias(1.0, 1.0, input2);
-				Rot4 random;
-				do
-				{
-					random = Rot4.Random;
-				}
-				while (random == Find.World.CoastDirectionAt(map.Tile));
-				if (random == Rot4.North)
-				{
-					input2 = new Rotate(0.0, 90.0, 0.0, input2);
-					input2 = new Translate(0.0, 0.0, -map.Size.z, input2);
-				}
-				else if (random == Rot4.East)
-				{
-					input2 = new Translate(-map.Size.x, 0.0, 0.0, input2);
-				}
-				else if (random == Rot4.South)
-				{
-					input2 = new Rotate(0.0, 90.0, 0.0, input2);
-				}
-				else
-				{
-					_ = random == Rot4.West;
-				}
-				NoiseDebugUI.StoreNoiseRender(input2, "mountain");
-				input = new Add(input, input2);
-				NoiseDebugUI.StoreNoiseRender(input, "elev + mountain");
-			}
-			float b = (map.TileInfo.WaterCovered ? 0f : float.MaxValue);
-			MapGenFloatGrid elevation = MapGenerator.Elevation;
-			foreach (IntVec3 allCell in map.AllCells)
-			{
-				elevation[allCell] = Mathf.Min(input.GetValue(allCell), b);
-			}
-			ModuleBase input3 = new Perlin(0.020999999716877937, 2.0, 0.5, 6, map.ConstantRandSeed, QualityMode.High);
-			input3 = new ScaleBias(0.5, 0.5, input3);
-			NoiseDebugUI.StoreNoiseRender(input3, "noiseFert base");
-			MapGenFloatGrid fertility = MapGenerator.Fertility;
-			foreach (IntVec3 allCell2 in map.AllCells)
-			{
-				fertility[allCell2] = input3.GetValue(allCell2);
-			}
-			return false;
-		}
-	}
+    //Saving this in case we need it for future projects, might do something with, e.g., soil fertility that dips after enough plantings and then slowly bounces back..
+
+    //[HarmonyPatch(typeof(GenStep_ElevationFertility), "Generate")]
+
+    [HarmonyPatch(typeof(MouseoverReadout), "MouseoverReadoutOnGUI")]
+    public class MouseoverReadoutOnGUITranspiler
+    {
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo labelMaker = AccessTools.Method(typeof(MouseoverReadoutOnGUITranspiler), "MakeLabelIfRequired");
+            FieldInfo BotLeft = AccessTools.Field(typeof(MouseoverReadout), "BotLeft");
+            var codes = new List<CodeInstruction>(instructions);
+            int num = 0;
+            bool skip = true;
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (num == 7 && skip)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_0).WithLabels(codes[i].labels);
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, BotLeft);
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+
+                    yield return new CodeInstruction(OpCodes.Call, labelMaker);
+                    yield return new CodeInstruction(OpCodes.Stloc_1);
+                    skip = false;
+                    yield return codes[i].WithLabels(new Label[] { });
+                    continue;
+                }
+                yield return codes[i];
+                if (codes[i].opcode == OpCodes.Stloc_1)
+                {
+                    num++;
+                }
+            }
+        }
+        public static float MakeLabelIfRequired(IntVec3 cell, Vector2 BotLeft, float num)
+        {
+            var comp = Find.CurrentMap.GetComponent<MapComponent_LakesCanFreeze>();
+            if (comp != null)
+            {
+                float rectY = num;
+                int ind = comp.map.cellIndices.CellToIndex(cell);
+
+                float ice = comp.iceGrid[ind];
+                if (ice > 0)
+                {
+                    Widgets.Label(new Rect(BotLeft.x, (float)UI.screenHeight - BotLeft.y - rectY, 999f, 999f), "Ice depth " + Math.Round(ice, 2).ToString());
+                    rectY += 19f;
+                }
+
+                return rectY;
+            }
+            return num;
+        }
+    }
+
+
+    //    static bool Prefix(Map map, GenStepParams parms)
+    //    {
+    //        NoiseRenderer.renderSize = new IntVec2(map.Size.x, map.Size.z);
+    //        ModuleBase input = new Perlin(0.020999999716877937, 2.0, 0.5, 6, map.ConstantRandSeed, QualityMode.High);
+    //        input = new ScaleBias(0.5, 0.5, input);
+    //        NoiseDebugUI.StoreNoiseRender(input, "elev base");
+    //        float num = 1f;
+    //        switch (map.TileInfo.hilliness)
+    //        {
+    //            case Hilliness.Flat:
+    //                num = MapGenTuning.ElevationFactorFlat;
+    //                break;
+    //            case Hilliness.SmallHills:
+    //                num = MapGenTuning.ElevationFactorSmallHills;
+    //                break;
+    //            case Hilliness.LargeHills:
+    //                num = MapGenTuning.ElevationFactorLargeHills;
+    //                break;
+    //            case Hilliness.Mountainous:
+    //                num = MapGenTuning.ElevationFactorMountains;
+    //                break;
+    //            case Hilliness.Impassable:
+    //                num = MapGenTuning.ElevationFactorImpassableMountains;
+    //                break;
+    //        }
+    //        input = new Multiply(input, new Const(num));
+    //        NoiseDebugUI.StoreNoiseRender(input, "elev world-factored");
+    //        if (map.TileInfo.hilliness == Hilliness.Mountainous || map.TileInfo.hilliness == Hilliness.Impassable)
+    //        {
+    //            ModuleBase input2 = new DistFromAxis((float)map.Size.x * 0.42f);
+    //            input2 = new Clamp(0.0, 1.0, input2);
+    //            input2 = new Invert(input2);
+    //            input2 = new ScaleBias(1.0, 1.0, input2);
+    //            Rot4 random;
+    //            do
+    //            {
+    //                random = Rot4.Random;
+    //            }
+    //            while (random == Find.World.CoastDirectionAt(map.Tile));
+    //            if (random == Rot4.North)
+    //            {
+    //                input2 = new Rotate(0.0, 90.0, 0.0, input2);
+    //                input2 = new Translate(0.0, 0.0, -map.Size.z, input2);
+    //            }
+    //            else if (random == Rot4.East)
+    //            {
+    //                input2 = new Translate(-map.Size.x, 0.0, 0.0, input2);
+    //            }
+    //            else if (random == Rot4.South)
+    //            {
+    //                input2 = new Rotate(0.0, 90.0, 0.0, input2);
+    //            }
+    //            else
+    //            {
+    //                _ = random == Rot4.West;
+    //            }
+    //            NoiseDebugUI.StoreNoiseRender(input2, "mountain");
+    //            input = new Add(input, input2);
+    //            NoiseDebugUI.StoreNoiseRender(input, "elev + mountain");
+    //        }
+    //        float b = (map.TileInfo.WaterCovered ? 0f : float.MaxValue);
+    //        MapGenFloatGrid elevation = MapGenerator.Elevation;
+    //        foreach (IntVec3 allCell in map.AllCells)
+    //        {
+    //            elevation[allCell] = Mathf.Min(input.GetValue(allCell), b);
+    //        }
+    //        ModuleBase input3 = new Perlin(0.020999999716877937, 2.0, 0.5, 6, map.ConstantRandSeed, QualityMode.High);
+    //        input3 = new ScaleBias(0.5, 0.5, input3);
+    //        NoiseDebugUI.StoreNoiseRender(input3, "noiseFert base");
+    //        MapGenFloatGrid fertility = MapGenerator.Fertility;
+    //        foreach (IntVec3 allCell2 in map.AllCells)
+    //        {
+    //            fertility[allCell2] = input3.GetValue(allCell2);
+    //        }
+    //        return false;
+    //    }
+    //}
 }
--- 1.3/Source/LakesCanFreeze/MapComponent_LakesCanFreeze.cs
+++ 1.3/Source/LakesCanFreeze/MapComponent_LakesCanFreeze.cs
@@ -11,9 +11,17 @@ namespace LCF
     public class MapComponent_LakesCanFreeze : MapComponent
     {
 		bool init;
-        TerrainDef[] waterGrid;
-		MapGenFloatGrid elevation;
-		MapGenFloatGrid fertility;
+        public TerrainDef[] waterGrid;
+		public float[] iceGrid;
+		//MapGenFloatGrid elevation;
+		//MapGenFloatGrid fertility;
+		float thresholdThinIce = 25;
+		float thresholdIce = 125;
+		float thresholdThickIce = 250;
+		float maximumIce = 400;
+		int freezingMultiplier = 4;
+		int thawingMultiplier = 2;
+		float iceRate = 2500;
 
 		public MapComponent_LakesCanFreeze(Map map) : base(map)
 		{
@@ -28,110 +36,192 @@ namespace LCF
 		public void Initialize()
         {
 			Log.Message("[Lakes Can Freeze] Initializing..");
-			Map fakeMap = new Map() { uniqueID = map.uniqueID, info = map.info, cellIndices = map.cellIndices }; //Super secret fake map.
-			new GenStep_ElevationFertility().Generate(fakeMap, new GenStepParams()); //Parms aren't used.
-			//Preserve these, when another map is gen'd they'll be discarded!
-			elevation = MapGenerator.Elevation;
-			fertility = MapGenerator.Fertility;
-
-			waterGrid = new TerrainDef[map.cellIndices.NumGridCells];
-			for (int i = 0; i < map.cellIndices.NumGridCells; i++)
+			//if (map.AgeInDays == 0)
+			//{
+			//	Map fakeMap = new Map() { uniqueID = map.uniqueID, info = map.info, cellIndices = map.cellIndices }; //Super secret fake map.
+			//	MapGenerator.mapBeingGenerated = fakeMap;
+			//	RockNoises.Init(fakeMap);
+			//	new GenStep_ElevationFertility().Generate(fakeMap, new GenStepParams()); //Parms aren't used.
+			//	RockNoises.Reset();
+			//	//Preserve these, when another map is gen'd they'll be discarded!
+			//	elevation = MapGenerator.Elevation;
+			//	fertility = MapGenerator.Fertility;
+			//	MapGenerator.mapBeingGenerated = map; //Put it back, don't be rude!
+			//}
+			if (waterGrid == null) //If we haven't got a waterGrid loaded from the save file, make one.
 			{
-				var c = map.cellIndices.IndexToCell(i);
-				var t = TerrainFrom(c, map, elevation[c], fertility[c], null, true);
-				if (t == TerrainDefOf.WaterDeep || t == TerrainDefOf.WaterShallow)
-					waterGrid[i] = t;
+				Log.Message("[LakesCanFreeze] Generating Water Grid..");
+				waterGrid = new TerrainDef[map.cellIndices.NumGridCells];
+				for (int i = 0; i < map.cellIndices.NumGridCells; i++)
+				{
+					var c = map.cellIndices.IndexToCell(i);
+					var t = c.GetTerrain(map);
+					if (t == TerrainDefOf.WaterDeep || t == TerrainDefOf.WaterShallow)
+						waterGrid[i] = t;
+				}
 			}
+			if (iceGrid == null)
+				iceGrid = new float[map.cellIndices.NumGridCells];
 			init = true;
 		}
 
-        public override void MapComponentTick()
-        {
+		public override void MapComponentTick()
+		{
 			if (!init) //If we aren't initialized..
 				Initialize(); //Initialize it!
-			if (Find.TickManager.TicksGame % 2500 != 0) //If it's not once per hour..
+			if (Find.TickManager.TicksGame % iceRate != 0) //If it's not once per hour..
 				return; //Don't execute the rest, throttling measure.
-			Log.Message("[LakesCanFreeze] Ticky tocky..");
+			int cellsFreezing = 0;
+			int cellsThawing = 0;
+			int cellsRefilled = 0;
+			int cellsThawed = 0;
 			for (int i = 0; i < waterGrid.Length; i++) //Thread this later probably.
 			{
-				var c = map.cellIndices.IndexToCell(i);
-				var ot = c.GetTerrain(map); //Get original terrain.
-				//If it's natural water or it's shallow/deep right now.
-				if (waterGrid[i] != null || ot == TerrainDefOf.WaterShallow || ot == TerrainDefOf.WaterDeep)
-						{ 
-				var temp = GenTemperature.GetTemperatureForCell(c, map);
-					if (temp < 0) //Temperature is below zero..
+				var cell = map.cellIndices.IndexToCell(i);
+				var currentTerrain = cell.GetTerrain(map); //Get current terrain.
+				//If it's lake ice or it's water..
+				if (currentTerrain == IceDefs.LCF_LakeIceThin || 
+					currentTerrain == IceDefs.LCF_LakeIce || 
+					currentTerrain == IceDefs.LCF_LakeIceThick || 
+					currentTerrain == TerrainDefOf.WaterShallow || 
+					currentTerrain == TerrainDefOf.WaterDeep)
+				{
+					var temperature = GenTemperature.GetTemperatureForCell(cell, map);
+					bool cellIsFreezing = false;
+					if (temperature < 0) //Temperature is below zero..
 					{
-						map.terrainGrid.SetUnderTerrain(c, ot); //Store it in under-terrain
-						map.terrainGrid.SetTerrain(c, TerrainDefOf.Ice); //Put ice on top
+						cellsFreezing++;
+						iceGrid[i] += -temperature * freezingMultiplier * //Based on temperature but sped up by a multiplier.
+							(currentTerrain == TerrainDefOf.WaterDeep ? .5f : 1f) * //If it's deep water right now, slow it down more.
+							(.75f + (.25f * Rand.Value)) //25% of the rate is variable for flavor.
+							/ 2500 * iceRate; //Adjust to iceRate based on the 2500 we tuned it to originally.
+						if (iceGrid[i] > maximumIce)
+							iceGrid[i] = maximumIce;
+						cellIsFreezing = true;
 					}
-					else if (temp > 0) //Temperature is above zero..
+					else if (temperature > 0) //Temperature is above zero..
 					{
-						TerrainDef ut = map.terrainGrid.UnderTerrainAt(c); //Get under-terrain
-						if (ut != null) //If there was under-terrain
+						TerrainDef underTerrain = map.terrainGrid.UnderTerrainAt(cell); //Get under-terrain
 						{
-							map.terrainGrid.SetTerrain(c, ut); //Set the top layer to the under-terrain
-							map.terrainGrid.SetUnderTerrain(c, null); //Clear the under-terrain
+							cellsThawing++;
+							iceGrid[i] -= temperature / thawingMultiplier * //Based on temperature but slowed down by a multiplier.
+								(currentTerrain == IceDefs.LCF_LakeIceThick ? .5f : 1f) *  //If it's thick ice right now, slow it down more.
+								(currentTerrain == IceDefs.LCF_LakeIce ? .75f : 1f) * //If it's regular ice right now, slow it down a little less than thick.
+								(.75f + (.25f * Rand.Value)) //25% of the rate is variable for flavor.
+								/ 2500 * iceRate; //Adjust to iceRate based on the 2500 we tuned it to originally.
+							if (iceGrid[i] < 0)
+								iceGrid[i] = 0;
+							if (iceGrid[i] == 0)
+							{
+								if (underTerrain != null && (underTerrain == TerrainDefOf.WaterShallow || underTerrain == TerrainDefOf.WaterDeep)) //If there was under-terrain and it's water.
+								{
+									cellsThawed++;
+									cellsThawing--; //Not thawing if it thawed!
+									map.terrainGrid.SetTerrain(cell, underTerrain); //Set the top layer to the under-terrain
+									map.terrainGrid.SetUnderTerrain(cell, null); //Clear the under-terrain
+								}
+								else if (waterGrid[i] != null && currentTerrain != TerrainDefOf.WaterDeep && currentTerrain != TerrainDefOf.WaterShallow) //If it's natural water but is missing..
+								{
+									var season = GenLocalDate.Season(map);
+									if (season == Season.Spring || season == Season.Summer || season == Season.PermanentSummer)
+									{
+										cellsRefilled++;
+										map.terrainGrid.SetTerrain(cell, waterGrid[i]); //Restore it.
+									}
+								}
+								DestroyBuildingsInCell(cell);
+							}
 						}
-						else if (waterGrid[i] != null) //If it's natural water..
-							map.terrainGrid.SetTerrain(c, waterGrid[i]); //Restore it.
-						//else it wasn't natural water, and we didn't freeze it, so leave it alone..?
+					}
+					//Update ice stage..
+					if (iceGrid[i] > thresholdThinIce && iceGrid[i] < thresholdIce)
+					{
+						map.terrainGrid.SetTerrain(cell, IceDefs.LCF_LakeIceThin); //Switch to thin ice.
+						if (cellIsFreezing) //If it's going down, *not* up..
+							map.terrainGrid.SetUnderTerrain(cell, currentTerrain); //Store the original in the under-terrain
+					}
+					else if (iceGrid[i] > thresholdIce && iceGrid[i] < thresholdThickIce)
+					{
+						map.terrainGrid.SetTerrain(cell, IceDefs.LCF_LakeIce); //Switch to regular ice.
+					}
+					else  if (iceGrid[i] > thresholdThickIce)
+					{
+						map.terrainGrid.SetTerrain(cell, IceDefs.LCF_LakeIceThick); //Switch to thick ice.
 					}
 				}
+			}
+			Log.Message("[LakesCanFreeze] Freezing " + cellsFreezing + ", thawing " + cellsThawing + ", thawed " + cellsThawed + ", and refilling " + cellsRefilled + ".");
+		}
+
+		public void DestroyBuildingsInCell(IntVec3 cell)
+        {
+			var things = cell.GetThingList(map);
+			for (int i = 0; i < things.Count; i++)
+			{
+				var thing = things[i];
+				if (thing.def is BuildableDef && thing.def.destroyable)
+					thing.Destroy(DestroyMode.Deconstruct);
 			}
         }
 
-		//Copypasta from GenStep_Terrain due to PRIVATE.
-		private TerrainDef TerrainFrom(IntVec3 c, Map map, float elevation, float fertility, RiverMaker river, bool preferSolid)
+        public override void ExposeData()
 		{
-			TerrainDef terrainDef = null;
-			if (river != null)
-			{
-				terrainDef = river.TerrainAt(c, recordForValidation: true);
-			}
-			if (terrainDef == null && preferSolid)
-			{
-				return GenStep_RocksFromGrid.RockDefAt(c).building.naturalTerrain;
-			}
-			TerrainDef terrainDef2 = BeachMaker.BeachTerrainAt(c, map.Biome);
-			if (terrainDef2 == TerrainDefOf.WaterOceanDeep)
-			{
-				return terrainDef2;
-			}
-			if (terrainDef != null && terrainDef.IsRiver)
-			{
-				return terrainDef;
-			}
-			if (terrainDef2 != null)
-			{
-				return terrainDef2;
-			}
-			if (terrainDef != null)
-			{
-				return terrainDef;
-			}
-			for (int i = 0; i < map.Biome.terrainPatchMakers.Count; i++)
-			{
-				terrainDef2 = map.Biome.terrainPatchMakers[i].TerrainAt(c, map, fertility);
-				if (terrainDef2 != null)
-				{
-					return terrainDef2;
-				}
-			}
-			if (elevation > 0.55f && elevation < 0.61f)
-			{
-				return TerrainDefOf.Gravel;
-			}
-			if (elevation >= 0.61f)
-			{
-				return GenStep_RocksFromGrid.RockDefAt(c).building.naturalTerrain;
-			}
-			terrainDef2 = TerrainThreshold.TerrainAtValue(map.Biome.terrainsByFertility, fertility);
-			if (terrainDef2 != null)
-			{
-				return terrainDef2;
-			}
-			return TerrainDefOf.Sand;
-		}
-	}
+			Scribe_Values.Look(ref waterGrid, "waterGrid");
+			Scribe_Values.Look(ref iceGrid, "iceGrid");
+			base.ExposeData();
+        }
+
+        ////Copypasta from GenStep_Terrain due to PRIVATE.
+        //private TerrainDef TerrainFrom(IntVec3 c, Map map, float elevation, float fertility, RiverMaker river, bool preferSolid)
+        //{
+        //	TerrainDef terrainDef = null;
+        //	if (river != null)
+        //	{
+        //		terrainDef = river.TerrainAt(c, recordForValidation: true);
+        //	}
+        //	if (terrainDef == null && preferSolid)
+        //	{
+        //		return null; //Changed from RockDefAt, we don't care about rocks here.
+        //	}
+        //	TerrainDef terrainDef2 = BeachMaker.BeachTerrainAt(c, map.Biome);
+        //	if (terrainDef2 == TerrainDefOf.WaterOceanDeep)
+        //	{
+        //		return terrainDef2;
+        //	}
+        //	if (terrainDef != null && terrainDef.IsRiver)
+        //	{
+        //		return terrainDef;
+        //	}
+        //	if (terrainDef2 != null)
+        //	{
+        //		return terrainDef2;
+        //	}
+        //	if (terrainDef != null)
+        //	{
+        //		return terrainDef;
+        //	}
+        //	for (int i = 0; i < map.Biome.terrainPatchMakers.Count; i++)
+        //	{
+        //		terrainDef2 = map.Biome.terrainPatchMakers[i].TerrainAt(c, map, fertility);
+        //		if (terrainDef2 != null)
+        //		{
+        //			return terrainDef2;
+        //		}
+        //	}
+        //	if (elevation > 0.55f && elevation < 0.61f)
+        //	{
+        //		return null; //We don't care about gravel.
+        //	}
+        //	if (elevation >= 0.61f)
+        //	{
+        //		return null; //We don't care about your rocks.
+        //	}
+        //	terrainDef2 = TerrainThreshold.TerrainAtValue(map.Biome.terrainsByFertility, fertility);
+        //	if (terrainDef2 != null)
+        //	{
+        //		return terrainDef2;
+        //	}
+        //	return TerrainDefOf.Sand;
+        //}
+    }
 }
