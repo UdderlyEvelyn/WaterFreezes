8c7eeb09f008441b42b944d933d324d553ffcc62
Evelyn <udderlyevelyn@gmail.com>
2022-03-02 17:46:35 -0500

    Added SetTerrain transpiler to fix issue with any terrain at worldgen being layerable.

--- 1.3/Source/WaterFreezes/Harmony Patches/TerrainGrid_SetTerrain.cs
+++ 1.3/Source/WaterFreezes/Harmony Patches/TerrainGrid_SetTerrain.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using RimWorld;
 using Verse;
 using HarmonyLib;
+using System.Reflection.Emit;
+using System.Reflection;
 
 namespace WF
 {
@@ -20,14 +22,38 @@ namespace WF
             //    WaterFreezes.Log("SetTerrain Prefix: Old terrain was \"" + (__state?.defName ?? "null") + "\", new terrain will be \"" + (newTerr?.defName ?? "null") + "\"..");
         }
 
+        internal static FieldInfo terrainDef_layerable = AccessTools.Field(typeof(TerrainDef), "layerable");
+        internal static FieldInfo terrainGrid_topGrid = AccessTools.Field(typeof(TerrainGrid), "topGrid");
+
+        //This fixes failures when attempting to place layerable terrain during worldgen (technically any time that the terrain at a place is null and it tries to place layerable terrain).
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var instructionList = instructions.ToList();
+            for (int i = 0; i < instructionList.Count; ++i)
+            {
+                var instruction = instructionList[i];
+                yield return instruction; //Send this one out.
+                if (instruction.opcode == OpCodes.Ldfld && instruction.OperandIs(terrainDef_layerable)) //TerrainDef.layerable
+                {
+                    yield return instructionList[i + 1]; //Send the next one out too.
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); //this.
+                    yield return new CodeInstruction(OpCodes.Ldfld, terrainGrid_topGrid); //TerrainGrid.topGrid
+                    yield return new CodeInstruction(OpCodes.Ldloc_0); //num (indexer)
+                    yield return new CodeInstruction(OpCodes.Ldelem_Ref); //[] (index int topGrid with indexer num)
+                    yield return new CodeInstruction(OpCodes.Brfalse_S, instructionList[i + 1].operand); //If it's not null, then..
+                    ++i; //Make sure we don't process that extra one we sent out.
+                }
+            }
+        }
+
         internal static void Postfix(IntVec3 c, TerrainDef newTerr, Map ___map, ref TerrainDef __state)
         {
             //if (__state != newTerr)
             //    WaterFreezes.Log("SetTerrain Postfix: Old terrain was \"" + (__state?.defName ?? "null") + "\", new terrain will be \"" + (newTerr?.defName ?? "null") + "\"..");
-            int i = ___map.cellIndices.CellToIndex(c);
             var oldTerrain = __state;
             if (oldTerrain == newTerr || oldTerrain == null) //If we're not actually changing anything or it had no terrain previously..
                 return; //Who cares?
+            int i = ___map.cellIndices.CellToIndex(c);
             if (oldTerrain.IsFreezableWater()) //If old terrain is freezable water..
             {
                 if (!newTerr.IsFreezableWater()) //It's water and becoming NOT water..
