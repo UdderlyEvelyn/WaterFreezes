1b127aa436cb71818bd10e84906f991e5443de20
Evelyn <udderlyevelyn@gmail.com>
2022-03-04 21:22:55 -0500

    Removed SetTerrain transpiler for layerable, removed two unnecessary newlines..

--- 1.3/Source/WaterFreezes/Harmony Patches/TerrainGrid_SetTerrain.cs
+++ 1.3/Source/WaterFreezes/Harmony Patches/TerrainGrid_SetTerrain.cs
@@ -22,39 +22,14 @@ namespace WF
             //    WaterFreezes.Log("SetTerrain Prefix: Old terrain was \"" + (__state?.defName ?? "null") + "\", new terrain will be \"" + (newTerr?.defName ?? "null") + "\"..");
         }
 
-        internal static FieldInfo terrainDef_layerable = AccessTools.Field(typeof(TerrainDef), "layerable");
-        internal static FieldInfo terrainGrid_topGrid = AccessTools.Field(typeof(TerrainGrid), "topGrid");
-
-        //This fixes failures when attempting to place layerable terrain during worldgen (technically any time that the terrain at a place is null and it tries to place layerable terrain).
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var instructionList = instructions.ToList();
-            for (int i = 0; i < instructionList.Count; ++i)
-            {
-                var instruction = instructionList[i];
-                yield return instruction; //Send this one out.
-                if (instruction.opcode == OpCodes.Ldfld && instruction.OperandIs(terrainDef_layerable)) //TerrainDef.layerable
-                {
-                    yield return instructionList[i + 1]; //Send the next one out too.
-                    yield return new CodeInstruction(OpCodes.Ldarg_0); //this.
-                    yield return new CodeInstruction(OpCodes.Ldfld, terrainGrid_topGrid); //TerrainGrid.topGrid
-                    yield return new CodeInstruction(OpCodes.Ldloc_0); //num (indexer)
-                    yield return new CodeInstruction(OpCodes.Ldelem_Ref); //[] (index int topGrid with indexer num)
-                    yield return new CodeInstruction(OpCodes.Brfalse_S, instructionList[i + 1].operand); //If it's not null, then..
-                    ++i; //Make sure we don't process that extra one we sent out.
-                }
-            }
-        }
-
         internal static void Postfix(IntVec3 c, TerrainDef newTerr, Map ___map, ref TerrainDef __state)
         {
             //if (__state != newTerr)
             //    WaterFreezes.Log("SetTerrain Postfix: Old terrain was \"" + (__state?.defName ?? "null") + "\", new terrain will be \"" + (newTerr?.defName ?? "null") + "\"..");
-            var oldTerrain = __state;
-            if (oldTerrain == newTerr || oldTerrain == null) //If we're not actually changing anything or it had no terrain previously..
+            if (__state == newTerr || __state == null) //If we're not actually changing anything or it had no terrain previously..
                 return; //Who cares?
             int i = ___map.cellIndices.CellToIndex(c);
-            if (oldTerrain.IsFreezableWater()) //If old terrain is freezable water..
+            if (__state.IsFreezableWater()) //If old terrain is freezable water..
             {
                 if (!newTerr.IsFreezableWater()) //It's water and becoming NOT water..
                 {
@@ -77,7 +52,7 @@ namespace WF
                 var comp = WaterFreezesCompCache.GetFor(___map);
                 if (comp == null || !comp.Initialized) //If comp is null or uninitialized..
                     return; //Don't try.
-                if (!oldTerrain.IsThawableIce() && newTerr.IsFreezableWater()) //But it's becoming water now..
+                if (!__state.IsThawableIce() && newTerr.IsFreezableWater()) //But it's becoming water now..
                 {
                     var naturalWater = comp.NaturalWaterTerrainGrid[i] != null;
                     if (!naturalWater && !newTerr.IsThawableIce()) //It's not natural water..
